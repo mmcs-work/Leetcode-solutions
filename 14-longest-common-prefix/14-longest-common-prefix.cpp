ca8f1de3967d7396827ea7c4bd13d551bd3b9027
Mainak Manna <moinak.manna@gmail.com>
2022-12-11 00:43:04 +0100

    Time: 38 ms (5.13%), Space: 10.2 MB (9.50%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==1) return strs[0];
+        map<string,int> m;
+        for(auto s:strs){
+            string p;
+            for(auto c:s){
+                p+=c;
+                m[p]++;
+            }
+        }
+        int ans = 0;
+        string an;
+        for(auto me:m){
+            if(me.second >= ans){
+                if(me.first.length()>an.length()){
+                    an=me.first;
+                    ans = me.second;
+                }
+            }
+        }
+        if(m[an] == strs.size())
+            return an;
+        else
+            return "";
+    }
+};
