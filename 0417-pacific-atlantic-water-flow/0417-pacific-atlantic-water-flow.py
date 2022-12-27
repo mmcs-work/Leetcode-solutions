bea9e0996d140995c8129d95e24d21953466c76c
Mainak Manna <moinak.manna@gmail.com>
2022-12-27 08:25:47 +0100

    Time: 277 ms (95.64%), Space: 15.4 MB (99.08%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,29 +1,29 @@
 class Solution:
-    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-        if not matrix or not matrix[0]:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
             return []
         
-        num_rows, num_cols = len(matrix), len(matrix[0])
+        nr, nc = len(heights), len(heights[0])
+        
         pacific_reachable = set()
         atlantic_reachable = set()
         
         def dfs(row,col, reachable):
             reachable.add((row,col))
             for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
-                new_row, new_col = row+x,col+y
-                if new_row < 0 or new_row >=num_rows or new_col>=num_cols or new_col<0:
+                nx, ny = row+x,col+y
+                if nx < 0 or nx>=nr or ny >=nc or ny<0:
                     continue
-                if (new_row,new_col) in reachable:
+                if (nx,ny) in reachable:
                     continue
-                if matrix[new_row][new_col] < matrix[row][col]:
+                if heights[nx][ny] < heights[row][col]:
                     continue
-                dfs(new_row,new_col,reachable)
+                dfs(nx,ny,reachable)
         
-        for i in range(num_rows):
+        for i in range(nr):
             dfs(i,0,pacific_reachable)
-            dfs(i,num_cols-1,atlantic_reachable)
-        for i in range(num_cols):
+            dfs(i,nc-1,atlantic_reachable)
+        for i in range(nc):
             dfs(0, i, pacific_reachable)
-            dfs(num_rows - 1, i, atlantic_reachable)
-        
+            dfs(nr - 1, i, atlantic_reachable)
         return list(pacific_reachable.intersection(atlantic_reachable))
