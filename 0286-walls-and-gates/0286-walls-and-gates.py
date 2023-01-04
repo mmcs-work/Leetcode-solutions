b5120c9236e6f60eeefc196c4993e189841f91f1
Mainak Manna <moinak.manna@gmail.com>
2023-01-04 17:33:49 +0100

    Time: 671 ms (45.84%), Space: 20.7 MB (30.03%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,21 +1,27 @@
 class Solution:
-    def wallsAndGates(self, r: List[List[int]]) -> None:
+    def wallsAndGates(self, g: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        q = deque()
-        m,n = len(r),len(r[0])
+        m,n = len(g),len(g[0])
+        q=deque()
+        inf = 2147483647 
+        vis=set()
         for i in range(m):
             for j in range(n):
-                if r[i][j]==0:
-                    q.append((i,j,0))
-        v = [[0]*n for _ in range(m)]
-        dv = v.copy()
-        INF=2147483647
-        while len(q)>0:
-            x,y,d = q.popleft()
-            v[x][y]=1
-            for p,pp in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-                if p>=0 and pp>=0 and p<m and pp<n and v[p][pp]==0 and r[p][pp]==INF:
-                    r[p][pp]=min(r[p][pp],d+1)
-                    q.append((p,pp,d+1))
+                if g[i][j]==0:
+                    q.append((i,j))
+                    vis.add((i,j))
+        dist = 0
+        while q:
+            nn = len(q)
+            for _ in range(nn):
+                x,y=q.popleft()
+                g[x][y]=dist
+                for di in [(-1,0),(1,0),(0,1),(0,-1)]:
+                    nx,ny=x+di[0],y+di[1]
+                    if nx>=0 and ny>=0 and nx<m and ny<n and (nx,ny) not in vis and g[nx][ny]==inf:
+                        q.append((nx,ny))
+                        vis.add((nx,ny))
+            dist+=1
+        return g
