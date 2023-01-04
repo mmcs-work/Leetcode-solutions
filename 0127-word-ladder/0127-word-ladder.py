b069e87a3259e28f0e5f6abac9e34bf79c949f7a
Mainak Manna <moinak.manna@gmail.com>
2023-01-04 20:45:20 +0100

    Time: 1301 ms (21.32%), Space: 15.1 MB (82.34%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -5,7 +5,7 @@ class Solution:
 
         if end not in word_list:
             return 0
-        word_list =set(word_list)
+        word_list = set(word_list)
         q = deque([begin])
         vis = set()
         vis.add(begin)
@@ -14,16 +14,17 @@ class Solution:
             nn = len(q)
             for _ in range(nn):
                 x = q.popleft()
-                if x == end:
+                if x in word_list and x == end:
                     return dist
                 for i in range(len(x)):
                     for p in range(ord('a'),ord('z')+1):
-                        s = x[0:i]+chr(p)+x[i+1:]
-                        if s not in vis and s in word_list:
-                            if s == end:
-                                return dist+1
-                            vis.add(s)
-                            q.append(s)
+                        if x[i] != chr(p):
+                            s = x[0:i]+chr(p)+x[i+1:]
+                            if s not in vis and s in word_list:
+                                if s == end:
+                                    return dist+1
+                                vis.add(s)
+                                q.append(s)
             dist+=1
 
         return 0        
