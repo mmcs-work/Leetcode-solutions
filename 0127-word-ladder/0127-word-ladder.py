ccad5385d151112fa535ab31d3f40eb08e78d080
Mainak Manna <moinak.manna@gmail.com>
2023-01-04 20:43:56 +0100

    Time: 1675 ms (11.54%), Space: 15.1 MB (82.34%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,30 +1,29 @@
 class Solution:
     def ladderLength(self, begin: str, end: str, word_list: List[str]) -> int:
-        if begin == end: return 1
-        unvisited_words = set(word_list)
+        if begin == end:
+            return 0
+
         if end not in word_list:
             return 0
-        def get_neighbors(word):
-            unvisited_neighbors = []
-            for i in range(len(word)):
-                # replace word[i] with every ascii letter
-                for c in ascii_letters:
-                    next_word = word[:i] + c + word[i + 1:]
-                    if next_word in unvisited_words:  
-                        unvisited_neighbors.append(next_word)
-                        unvisited_words.remove(next_word) # mark next_word as visited
-            return unvisited_neighbors
+        word_list =set(word_list)
+        q = deque([begin])
+        vis = set()
+        vis.add(begin)
+        dist = 1
+        while q:
+            nn = len(q)
+            for _ in range(nn):
+                x = q.popleft()
+                if x == end:
+                    return dist
+                for i in range(len(x)):
+                    for p in range(ord('a'),ord('z')+1):
+                        s = x[0:i]+chr(p)+x[i+1:]
+                        if s not in vis and s in word_list:
+                            if s == end:
+                                return dist+1
+                            vis.add(s)
+                            q.append(s)
+            dist+=1
 
-        queue = deque([begin])
-        if begin in unvisited_words:
-            unvisited_words.remove(begin) #set begin as visited, nocontain itself
-        distance = 1
-        while len(queue) > 0:   # if current level is non-empty
-            n = len(queue)      # current level contains n nodes
-            distance += 1
-            for _ in range(n):  # only visit the nodes in the current level to keep track of distance
-                word = queue.popleft()
-                for w in get_neighbors(word):
-                    if w == end: return distance
-                    queue.append(w)
-        return 0   # word ladder not found
+        return 0        
