6684010e47386ae84bc344f5d1a8d5d0e0bba99b
Mainak Manna <moinak.manna@gmail.com>
2023-01-22 00:04:58 +0100

    Time: 79 ms (5.00%), Space: 14.1 MB (11.93%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,32 +1,5 @@
 class Solution:
-    def closetTarget(self, w: List[str], t: str, s: int) -> int:
-        n = len(w)
-        m = []
-        ss = s
-        i = s+1
-        ct = 1
-        i%=n
+    def closetTarget(self, w: List[str], t: str, ss: int) -> int:
         if t not in w:
             return -1
-        if w[s]==t:
-            return 0
-        while i!=s:
-            if w[i]==t:
-                m.append(ct)
-                break
-            i+=1
-            i%=n
-            ct+=1
-        i = s-1
-        ct = 1
-        i%=n
-        while i!=s:
-            if w[i] ==t:
-                m.append(ct)
-                break
-            i-=1
-            if i==-1:
-                i = n-1
-            ct+=1
-        return min(m)
-    
+        return min((w[ss:] + w).index(t), (w + w[:ss])[::-1].index(t) + 1)
