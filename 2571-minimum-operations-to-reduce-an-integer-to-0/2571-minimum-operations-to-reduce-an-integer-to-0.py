26b741d2e82a53b0611ef7207e6ef100e6b7328e
Mainak Manna <moinak.manna@gmail.com>
2023-02-22 21:52:42 +0100

    Time: 42 ms (15.51%), Space: 13.9 MB (55.90%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.py
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.py
@@ -1,3 +1,7 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        return (n ^ (n * 3)).bit_count()
+        def dp(x):
+            if x == 0: return 0
+            if (x & (x - 1)) == 0: return 1
+            return min(dp(x + (x & -x)), dp(x - (x & -x))) + 1
+        return dp(n)
